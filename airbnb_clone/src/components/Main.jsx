01c7bcda65b55449c69bbaa23d9d2c90f4095a33
Umida <umidakhongaybullaeva@gmail.com>
2022-11-03 11:45:06 -0400

    Pushing my next project

---
+++ airbnb_clone/src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main(props) {
+    return (
+        <div>
+            <h1>I am the Main Content</h1>
+        </div>
+    );
+}
+
+export default Main;
---
+++ airbnb_clone/src/components/Reservation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Reservation(props) {
+    return (
+        <div>
+           <h1> I am a Reservation Page </h1>
+        </div>
+    );
+}
+
+export default Reservation;
